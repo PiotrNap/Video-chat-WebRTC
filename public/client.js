54e1cd401c42be0d0ab6880106e475d85a98b27d
Piotr <piotr.napierala@intracto.com>
2021-03-27 16:31:20 +0100

    Support for second media track

--- public/client.js
+++ public/client.js
@@ -11,14 +11,15 @@ var form = document.querySelector("#form"),
 
 const iceServers = {
   iceServer: {
-    urls: "stun:stun.l.google.com:19302",
-  },
+    urls: "stun:stun.l.google.com:19302"
+  }
 };
 
 const pc = new RTCPeerConnection(iceServers);
 const socket = io();
 
 var rooms = [];
+var inboundStream = null;
 var localeStream, currentRoom;
 
 form.onsubmit = function (e) {
@@ -39,64 +40,77 @@ connect.onclick = function () {
   }
   // get user media
   navigator.mediaDevices
-    .getUserMedia({ video: true, audio: false })
-    .then((stream) => {
+    .getUserMedia({ video: true, audio: true })
+    .then(async stream => {
       client.srcObject = stream;
-      client.play();
-      socket.emit("join", currentRoom);
-      localeStream = stream;
+      try {
+        await client.play();
+        socket.emit("join", currentRoom);
+        localeStream = stream;
+      } catch (err) {
+        console.error(err);
+      }
     });
   dashboard.style.display = "none";
   stream.style.display = "block";
 };
 
-socket.on("room", (room) => {
+socket.on("room", room => {
   rooms.push(room);
   var li = document.createElement("li");
   li.innerText = room;
   roomsList.append(li);
 });
 
-socket.on("join", (room) => {
+socket.on("join", room => {
   if (currentRoom !== room) return;
 
   pc.ontrack = addRemoteMediaStream;
   pc.onicecandidate = generateIceCandidate;
   pc.addTrack(localeStream.getTracks()[0], localeStream);
-  pc.createOffer().then((description) => {
+  pc.addTrack(localeStream.getTracks()[1], localeStream);
+  pc.createOffer().then(description => {
     pc.setLocalDescription(description);
     console.log("Setting locale description:", description);
     socket.emit("offer", description);
   });
 });
 
-socket.on("offer", (offer) => {
+socket.on("offer", offer => {
   pc.ontrack = addRemoteMediaStream;
   pc.onicecandidate = generateIceCandidate;
   pc.setRemoteDescription(new RTCSessionDescription(offer));
   pc.addTrack(localeStream.getTracks()[0], localeStream);
-  pc.createAnswer().then((description) => {
+  pc.addTrack(localeStream.getTracks()[1], localeStream);
+  pc.createAnswer().then(description => {
     pc.setLocalDescription(description);
-    console.log("Setting locale description");
+    console.log("Setting locale description", description);
     socket.emit("answer", description);
   });
 });
 
-socket.on("answer", (answer) => {
+socket.on("answer", answer => {
   pc.setRemoteDescription(new RTCSessionDescription(answer));
 });
 
-socket.on("candidate", (event) => {
+socket.on("candidate", event => {
   var iceCandidate = new RTCIceCandidate({
     sdpMLineIndex: event.label,
-    candidate: event.candidate,
+    candidate: event.candidate
   });
   pc.addIceCandidate(iceCandidate);
 });
 
 function addRemoteMediaStream(event) {
-  guest.srcObject = event.streams[0];
-  guest.play();
+  if (event.streams && event.streams[0]) {
+    guest.srcObject = event.streams[0];
+  } else {
+    if (!inboundStream) {
+      inboundStream = new MediaStream();
+      guest.srcObject = inboundStream;
+    }
+    inboundStream.addTrack(event.track);
+  }
 }
 
 function generateIceCandidate(event) {
@@ -105,7 +119,7 @@ function generateIceCandidate(event) {
       type: "candidate",
       label: event.candidate.sdpMLineIndex,
       id: event.candidate.sdpMid,
-      candidate: event.candidate.candidate,
+      candidate: event.candidate.candidate
     };
     console.log("Sending a candidate: ", candidate);
     socket.emit("candidate", candidate);
